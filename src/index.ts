889ff496cbcdcd0ff4e9cd9fec1a40f2229eb2f8
diffshare <diffshare@gmail.com>
2023-02-22 10:30:01 +0900

    elastic searchにデータ送信

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import TuyAPI from 'tuyapi';
+import TuyAPI, { DPSObject } from 'tuyapi';
+import {Client} from '@elastic/elasticsearch';
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.DEVICE_ID ||
-  !process.env.DEVICE_KEY) {
-    throw new Error("id and key required.");
-
-  }
+if (!process.env.DEVICE_ID || !process.env.DEVICE_KEY) {
+  throw new Error("id and key required.");
+}
+if (!process.env.ELASTIC_USERNAME || !process.env.ELASTIC_PASSWORD) {
+  throw new Error("username and password required.");
+}
 
 const device = new TuyAPI({
   id: process.env.DEVICE_ID,
   key: process.env.DEVICE_KEY,
   version: '3.3',
   issueRefreshOnConnect: true});
-
-let stateHasChanged = false;
 
 // Find device on network
 device.find().then(() => {
@@ -35,28 +35,48 @@ device.on('error', error => {
   console.log('Error!', error);
 });
 
+const client = new Client({
+  node: {
+    url: new URL('https://elastic.diffshare.com/'),
+  },
+  auth: {
+    username: process.env.ELASTIC_USERNAME,
+    password: process.env.ELASTIC_PASSWORD,
+  }
+});
+// (async () => {
+//   const info = await client.info();
+//   console.log("info:", info);
+// })()
+const index = async (data: DPSObject) => {
+  if (!data.dps['19']) return;
+
+  const name = "洗濯機_power";
+  const mA = data.dps['18'];
+  const W  = data.dps['19'] as number / 10;
+  console.log(name, mA, 'mA', W, 'W');
+  await client.index({
+    index: name,
+    refresh: true,
+    document: {
+      value: W,
+      timestamp: new Date(),
+    }
+  })
+};
+
+// 更新時のデータ
 device.on('dp-refresh', data => {
-  // console.log('DP_REFRESH data from device: ', data);
-  console.log(data.dps['18'], 'mA', data.dps['19'] as number / 10, 'W');
+  index(data);
 });
 
+// デバイス接続時のデータ
 device.on('data', data => {
   console.log('Data from device:', data);
-
-  // console.log(`Boolean status of default property: ${data.dps['1']}.`);
-  console.log(data.dps['18'], 'mA', data.dps['19'] as number / 10, 'W');
-
-  /*
-  // Set default property to opposite
-  if (!stateHasChanged) {
-    device.set({dps: 1, set: !(data.dps['1'])});
-
-    // Otherwise we'll be stuck in an endless
-    // loop of toggling the state.
-    stateHasChanged = true;
-  }
-  */
+  index(data);
 });
 
-// Disconnect after 10 seconds
-// setTimeout(() => { device.disconnect(); }, 10000);
+// 定期的に更新して定期的にデータを取得する
+setInterval(() => {
+  device.refresh({requestedDPS: [18, 19]});
+}, 2000);
