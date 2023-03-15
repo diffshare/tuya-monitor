e8620f7047d3fd480e9f1c14efb2a5f0f4fc38f2
diffshare <diffshare@gmail.com>
2023-03-15 22:12:15 +0900

    commit忘れ

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,103 @@
 import TuyAPI, { DPSObject } from 'tuyapi';
-import {Client} from '@elastic/elasticsearch';
+import { Client } from '@elastic/elasticsearch';
+import devices from '../devices';
 import dotenv from 'dotenv';
 dotenv.config();
 
 if (!process.env.DEVICE_ID || !process.env.DEVICE_KEY) {
   throw new Error("id and key required.");
 }
-if (!process.env.ELASTIC_USERNAME || !process.env.ELASTIC_PASSWORD) {
-  throw new Error("username and password required.");
-}
 
-const device = new TuyAPI({
-  id: process.env.DEVICE_ID,
-  key: process.env.DEVICE_KEY,
-  version: '3.3',
-  issueRefreshOnConnect: true});
-
-// Find device on network
-device.find().then(() => {
-  // Connect to device
-  device.connect();
-});
-
-// Add event listeners
-device.on('connected', () => {
-  console.log('Connected to device!');
-});
-
-device.on('disconnected', () => {
-  console.log('Disconnected from device.');
-});
-
-device.on('error', error => {
-  console.log('Error!', error);
-});
-
-const client = new Client({
-  node: {
-    url: new URL('https://elastic.diffshare.com/'),
-  },
-  auth: {
-    username: process.env.ELASTIC_USERNAME,
-    password: process.env.ELASTIC_PASSWORD,
-  }
-});
-// (async () => {
-//   const info = await client.info();
-//   console.log("info:", info);
-// })()
-const index = async (data: DPSObject) => {
-  if (!data.dps['19']) return;
-
-  const name = "洗濯機_power";
-  const mA = data.dps['18'];
-  const W  = data.dps['19'] as number / 10;
-  console.log(name, mA, 'mA', W, 'W');
-  await client.index({
-    index: name,
-    refresh: true,
-    document: {
-      value: W,
-      timestamp: new Date(),
-    }
-  })
+type Device = {
+  name: string;
+  id: string;
+  key: string;
 };
 
-// 更新時のデータ
-device.on('dp-refresh', data => {
-  index(data);
-});
+const observe = (dev: Device) => {
+  if (!process.env.ELASTIC_USERNAME || !process.env.ELASTIC_PASSWORD) {
+    throw new Error("username and password required.");
+  }
+  const device = new TuyAPI({
+    id: dev.id,
+    key: dev.key,
+    version: '3.3',
+    issueRefreshOnConnect: true
+  });
 
-// デバイス接続時のデータ
-device.on('data', data => {
-  console.log('Data from device:', data);
-  index(data);
-});
+  // Find device on network
+  device.find({timeout: 10 * 60}).then(() => {
+    // Connect to device
+    device.connect();
+  });
 
-// 定期的に更新して定期的にデータを取得する
-setInterval(() => {
-  device.refresh({requestedDPS: [18, 19]});
-}, 2000);
+  // Add event listeners
+  device.on('connected', () => {
+    console.log('Connected to device!');
+  });
+
+  device.on('disconnected', () => {
+    console.log('Disconnected from device.');
+  });
+
+  device.on('error', error => {
+    console.log('Error!', error);
+  });
+
+  const client = new Client({
+    node: {
+      url: new URL('https://elastic.diffshare.com/'),
+    },
+    auth: {
+      username: process.env.ELASTIC_USERNAME,
+      password: process.env.ELASTIC_PASSWORD,
+    }
+  });
+  const index = async (data: DPSObject) => {
+    if (Number.isNaN(data.dps['19'])) return;
+
+    const name = `${dev.name}_power`;
+    const mA = Math.round(data.dps['18'] as number * 10) / 10;
+    const W  = Math.round(data.dps['19'] as number / 10 * 10) / 10;
+    console.log(name, mA, 'mA', W, 'W');
+    try {
+      await client.index({
+        index: name,
+        refresh: true,
+        document: {
+          value: W,
+          timestamp: new Date(),
+        }
+      })
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // 更新時のデータ
+  device.on('dp-refresh', data => {
+    index(data);
+  });
+
+  // デバイス接続時のデータ
+  device.on('data', data => {
+    console.log('Data from device:', data);
+    index(data);
+  });
+
+  // 定期的に更新して定期的にデータを取得する
+  setInterval(() => {
+    device.refresh({ requestedDPS: [18, 19] });
+  }, 3000);
+};
+
+devices.forEach(device => {
+  while (true)
+    try {
+      observe(device);
+      break;
+    }
+    catch {
+
+    }
+});
